214144dcebb5f8dc4bb35255560c47e83a92169f
Jakob Persons <jakobep24@gmail.com>
2020-12-27 12:43:22 -0500

    home page sorted by date and given border

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,13 @@
 class WelcomeController < ApplicationController
 
   def home
-    @events = Event.all
+    @events = sort_events_by_date
   end
 
   private
 
   def sort_events_by_date
+    Event.all.sort_by{ |event| event.date }
   end
 
 end
