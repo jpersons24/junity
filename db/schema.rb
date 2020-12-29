a9ad4193b979e70e2182f75ac911709b42f00cec
Jakob Persons <jakobep24@gmail.com>
2020-12-29 13:02:59 -0500

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_194149) do
+ActiveRecord::Schema.define(version: 2020_12_29_175913) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2020_12_24_194149) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
